2089d05d09ca5c47684518ba3e74e887d3919cb6
Danish-Ehsan <danish.ehsan321@gmail.com>
2022-07-14 16:10:28 -0400

    Testing service worker

--- js/install.js
+++ js/install.js
@@ -1,3 +1,13 @@
+// Register service worker to control making site work offline
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('js/service-worker.js')
+      .then(() => { console.log('Service Worker Registered'); });
+}
+  
+// Code to handle install prompt on desktop
+
 let deferredPrompt;
 const addBtn = document.querySelector('.js--addbtn');
 addBtn.style.display = 'none';
